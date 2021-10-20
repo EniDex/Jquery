c1e6bbffc7c8202a1529a4877235e5d7de152a8d
[EniDex] <[obirijedestiny@gmail.com]>
2021-10-20 09:48:01 +0100

    update new

---
+++ jquery.js
@@ -0,0 +1,8 @@
+ $(document).ready(function(){
+    $("button").hide(2000).addClass("border").show(2000)
+ });
+
+
+ $(document).ready(function(){
+    $("table").hide(3000).addClass("border").show(3000)
+ });
